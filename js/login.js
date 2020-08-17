4b2209d63d2ff54becc25bdd8aac532a4b72fbf0
Agustina <agustinamartinez1044@users.noreply.github.com>
2020-08-17 14:54:32 -0300

    Entrega1

--- js/login.js
+++ js/login.js
@@ -2,5 +2,10 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    document.getElementById('userinfo').addEventListener('submit', (evento)=> {
+        evento.preventDefault();
+        location.href = "./index.html";
+        sessionStorage.setItem("logged", true);
+        return true;
+    })
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,48 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+getJSONData(PRODUCTS_URL)
+    .then(function (resultado) {
 
+        let datos = resultado.data;
+        console.log(datos);
+
+        let html = "";
+        for (let i = 0; i < datos.length; i++) {
+            let dat = datos[i];
+            html += `
+            <a href="products-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + dat.imgSrc + `" alt="` + dat.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ dat.name + " - " + dat.currency + " "+ dat.cost +`</h4>
+                            <small class="text-muted">` + dat.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + dat.description  +`</p>
+                        
+                    </div>
+                </div>
+            </a>`
+        document.getElementById("contenedor").innerHTML = html;
+    }
+})
+
+        
+document.addEventListener("DOMContentLoaded", function (e) {
 });
+
+
+//OTRA FORMA
+/*fetch(url)
+    .then(function (respuesta) {
+        return respuesta.json();
+    })
+    .then(function (datos) {
+        for (let i = 0; i < datos.all.leght; i++) {
+            document.getElementById("contenedor").innerHTML += '<p>' + datos.all[i].text + '</p>';
+        }
+    })
+*/
